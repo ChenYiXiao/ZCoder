82e168a5a05939c4d8df4042c124da368d809167
chenyixiao <yhtt2020@foxmail.com>
2012-10-27 21:34:45 +0800

    母版页加入管理员权限验证

--- admin/MasterPage.master.cs
+++ admin/MasterPage.master.cs
@@ -9,6 +9,14 @@ public partial class admin_MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (UserOperation.CheckLoged(Session) == false)
+        {
+            SmallScript.goRedirect(Response, Session, "请先登陆。", "~/login.aspx");
+            return;
+        }
+        if (UserOperation.CheckIsAdmin(Session) == false )
+        {
+            SmallScript.goRedirect(Response, Session, "对不起，您并不具备使用后台的权限。", "~/default.aspx");
+        }
     }
 }
