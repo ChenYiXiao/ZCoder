802c970ae24fcc53f6a7fd6dac8e1de5794d60a7
chenbin <wzdxc123@163.com>
2012-10-28 21:00:45 +0800

    修改
    Signed-off-by: chenbin <wzdxc123@163.com>

--- admin/AddUser.aspx.cs
+++ admin/AddUser.aspx.cs
@@ -13,13 +13,20 @@ public partial class admin_AddUser : System.Web.UI.Page
     }
     protected void A_Button1_Click(object sender, EventArgs e)
     {
-
+        DataBase db = new DataBase();
+        String str1 = this.A_TB1.Text;
+        String str2 = A_TB2.Text;
+        String str3 = A_TB4.Text;
+        String str4 = "false";
+        if (A_CB1.Checked == true) {
+            str4 = "true";
+        }
+        String str="INSERT INTO dbo.tb_user VALUES(str1,str2,str3,null,null,str4,null)";
+        db.ExCommandNoBack(str);
     }
-    protected void A_Button2_Click(object sender, EventArgs e)
+
+    protected void A_TB1_TextChanged(object sender, EventArgs e)
     {
-        A_TB1.Text = null;
-        A_TB2.Text = null;
-        A_TB3.Text = null;
-        A_CB1.Checked = false;
+
     }
 }
