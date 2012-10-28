59ef2f7861222501e85e1beabc06d439d8e2f58b
chenbin <wzdxc123@163.com>
2012-10-28 21:41:54 +0800

    后台增加用户功能实现

--- admin/AddUser.aspx.cs
+++ admin/AddUser.aspx.cs
@@ -14,15 +14,21 @@ public partial class admin_AddUser : System.Web.UI.Page
     protected void A_Button1_Click(object sender, EventArgs e)
     {
         DataBase db = new DataBase();
-        String str1 = this.A_TB1.Text;
-        String str2 = A_TB2.Text;
-        String str3 = A_TB4.Text;
-        String str4 = "false";
-        if (A_CB1.Checked == true) {
-            str4 = "true";
+        string userName = this.A_TB1.Text;
+        string password = A_TB2.Text;
+        string email = A_TB4.Text;
+        bool isAdmin = false;
+        isAdmin = this.A_CB1.Checked;
+        if (UserOperation.Reg(userName, password, email, "", true, isAdmin, DateTime.Now))
+        {
+            SmallScript.MessageBox(Page, "添加用户成功。");
+            return;
         }
-        String str="INSERT INTO dbo.tb_user VALUES(str1,str2,str3,null,null,str4,null)";
-        db.ExCommandNoBack(str);
+        else
+        {
+            SmallScript.MessageBox(Page, "添加用户失败。");
+            return;
+        }
     }
 
     protected void A_TB1_TextChanged(object sender, EventArgs e)
