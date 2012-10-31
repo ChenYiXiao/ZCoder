4ffdc1b3d75eaa9e701cb283e3d5ce518fec5fd4
chenyixiao <yhtt2020@foxmail.com>
2012-10-31 16:22:40 +0800

    新增删除用户界面，业务逻辑未实现

---
+++ admin/DelUser.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class admin_DelUser : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            UserEntity ue = UserOperation.GetUser(Request.QueryString["id"].ToString());
+            if (ue != null)
+            {
+                lb_UserName.Text = ue.UserName;
+                lb_Id.Text = ue.Id.ToString();
+                lb_Email.Text = ue.Email;
+                lb_Sex.Text = ue.Sex?"男":"女";
+                lb_IsAdmin.Text = ue.Isadmin ? "管理员" : "用户";
+            }
+        }
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/admin/userlist.aspx");
+    }
+}
