27f6c4bf8efc97a44299f461c537be56128db3ae
chenyixiao <yhtt2020@foxmail.com>
2012-11-01 18:48:48 +0800

    新增工程实体类

---
+++ App_Code/Entity/ProjectEntity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+///ProjectEntity 的摘要说明
+/// </summary>
+public class ProjectEntity
+{
+    int id = -1;
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+    string projectName = "";
+
+    public string ProjectName
+    {
+        get { return projectName; }
+        set { projectName = value; }
+    }
+    int uid = -1;
+
+    public int Uid
+    {
+        get { return uid; }
+        set { uid = value; }
+    }
+    string description = "";
+
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+    DateTime upTime = DateTime.Now;
+
+    public DateTime UpTime
+    {
+        get { return upTime; }
+        set { upTime = value; }
+    }
+    int tid = -1; 
+
+    public int Tid
+    {
+        get { return tid; }
+        set { tid = value; }
+    }
+	
+}
