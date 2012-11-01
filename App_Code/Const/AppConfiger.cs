287620248ad134ac4065c3daf4ff3147df4d532f
chenyixiao <yhtt2020@foxmail.com>
2012-11-01 15:20:05 +0800

    独立配置文件config并完成调用类AppConfiger.cs

---
+++ App_Code/Const/AppConfiger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+/// <summary>
+///AppConfiger 的摘要说明
+/// </summary>
+public class AppConfiger
+{
+    public const string SiteConfigPath = "/config/site.config";
+    public AppConfiger()
+    {
+        //
+        //TODO: 在此处添加构造函数逻辑
+        //
+    }
+    public static string GetSiteSetting(System.Web.HttpServerUtility server, string name)
+    {
+        ConfigXmlDocument cfxd = new ConfigXmlDocument();
+        cfxd.Load(server.MapPath(SiteConfigPath));
+        try
+        {
+            string value = cfxd.GetElementsByTagName(name)[0].InnerText;
+            return value;
+        }
+        catch
+        {
+            return "";
+        }
+    }
+}
