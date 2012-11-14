2a9539f5101ccfc997956ea229f5372e96503516
XiaoJunTao <531201650@qq.com>
2012-11-14 16:06:04 +0800

    后台查看注释列表

---
+++ admin/NoteList.aspx.cs
@@ -0,0 +1,28 @@
+﻿/***************************************************************************************************
+ *
+ *\File          NoteList.aspx.cs
+ *\Description   后台查看注释列表
+ *\Log           2012.11.14    Ver 1.0    肖骏涛
+ *               创建文件。
+ ***************************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class admin_NoteList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+    }
+    protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+    {
+
+    }
+}
