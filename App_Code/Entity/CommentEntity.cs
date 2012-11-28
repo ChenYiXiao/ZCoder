4eecef3cd172b68853357a38e6f06c20baa5e474
chenbin <wzdxc123@163.com>
2012-11-28 21:56:46 +0800

    新增评论的类

---
+++ App_Code/Entity/CommentEntity.cs
@@ -0,0 +1,69 @@
+﻿/***************************************************************************************************
+ *
+ *\File          CommentEntity.cs
+ *\Description   
+ *\Log           2012.11.28    Ver 1.0    
+ *               创建文件。
+ ***************************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+///CommentEntity 的摘要说明
+/// </summary>
+public class CommentEntity
+{
+    int Id = -1;
+    public int id {
+        get { return id; }
+        set { id = value; }
+    }
+
+    String commentTitle = "";
+    public String CommetTitle {
+        get { return CommetTitle; }
+        set { CommetTitle = value; }
+    }
+
+    String conText = "";
+    public String ConText {
+        get { return ConText; }
+        set { ConText = value; }
+    }
+
+    int Agree = -1;
+    public int agree {
+        get { return agree; }
+        set { agree = value; }
+    }
+
+    int Disagree = -1;
+    public int DisAgree
+    {
+        get { return DisAgree; }
+        set { DisAgree = value; }
+    }
+
+    int Uid = -1;
+    public int uid {
+        get { return uid; }
+        set { uid = value; }
+    }
+
+    int Nid = -1;
+    public int nid
+    {
+        get { return nid; }
+        set { nid = value; }
+    }
+
+    DateTime upTime = DateTime.Now;
+
+    public DateTime UpTime
+    {
+        get { return upTime; }
+        set { upTime = value; }
+    }
+}
