c80081e3c9307b968be1dc1c473e96f81d9a75c8
陈一枭 <yhtt2020@foxmail.com>
2012-11-29 09:46:08 +0800

    对邮箱验证，不合理邮箱不给通过修改~

--- toAlert.aspx.cs
+++ toAlert.aspx.cs
@@ -59,31 +59,40 @@ public partial class Default2 : System.Web.UI.Page
             }
         }
     }
-    protected void btnEdit_Click(object sender, EventArgs e) 
+    protected void btnEdit_Click(object sender, EventArgs e)
     {
-        DataBase db1 = new DataBase(); /*实例化一个数据库*/
-        string ID = Session["uid"].ToString(); /*获取用户ID*/
-        /*获取用户填写信息*/
-        bool Sex = (rb_Sex.SelectedIndex == 0);
-        string Email = tb_Email.Text;
-        string QQ = tb_QQ.Text;
-        /*执行数据库更新语句*/
-        /*将修改好的个人资料录入数据库*/
-        string sql = "UPDATE tb_user SET  email ='" + Email + "', sex =" + (Sex ? 1 : 0).ToString() + ",  qq ='" + QQ + "' where id=" + ID.ToString();
-        try
+        Validate();
+        if (!Page.IsValid)
         {
-            db1.ExCommandNoBack(sql);
-        }
-        catch
-        {
-            SmallScript.MessageBox(Page, "修改失败！"); /*修改成功，页面跳出提示*/
+            SmallScript.MessageBox(Page, "修改失败！"); /*修改失败，页面跳出提示*/
+            /*如果页面验证没通过则返回*/
             return;
         }
+        else
+        {
+            DataBase db1 = new DataBase(); /*实例化一个数据库*/
+            string ID = Session["uid"].ToString(); /*获取用户ID*/
+            /*获取用户填写信息*/
+            bool Sex = (rb_Sex.SelectedIndex == 0);
+            string Email = tb_Email.Text;
+            string QQ = tb_QQ.Text;
+            /*执行数据库更新语句*/
+            /*将修改好的个人资料录入数据库*/
+            string sql = "UPDATE tb_user SET  email ='" + Email + "', sex =" + (Sex ? 1 : 0).ToString() + ",  qq ='" + QQ + "' where id=" + ID.ToString();
+            try
+            {
+                db1.ExCommandNoBack(sql);
+            }
+            catch
+            {
+                SmallScript.MessageBox(Page, "修改失败！"); /*修改失败，页面跳出提示*/
+                return;
+            }
 
-        SmallScript.MessageBox(Page, "修改用户成功。"); /*修改失败，页面跳出提示*/
-        return;
+            SmallScript.MessageBox(Page, "修改用户成功。"); /*修改成功，页面跳出提示*/
+            return;
+        }
     }
-
     protected void tb_Password_TextChanged(object sender, EventArgs e)
     {
 
