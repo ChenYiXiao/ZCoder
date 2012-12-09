fa580b8b37788ec452ccac345ffb2fa768a24ac8
chenyixiao <yhtt2020@foxmail.com>
2012-12-09 19:45:48 +0800

    增加读取注释的页面

---
+++ LoadNotes.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class LoadNotes : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        List<NoteEntity> assNotes= NoteOperation.GetNotesBySartLine(3, 6199);
+        foreach(NoteEntity ne in  assNotes)
+        {
+           Response.Write(ne.Context);
+        }
+    }
+}
