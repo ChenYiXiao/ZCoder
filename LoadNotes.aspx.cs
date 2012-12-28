c125d62bde21c02842b118923b07d5ae7d744510
xjw129xjt <531201650@qq.com>
2012-12-28 13:00:37 +0800

    .

--- LoadNotes.aspx.cs
+++ LoadNotes.aspx.cs
@@ -17,8 +17,8 @@ public partial class LoadNotes : System.Web.UI.Page
         int startline = int.Parse(Request.QueryString["startline"].ToString());
         int cid = int.Parse(Request.QueryString["cid"].ToString());
         assNotes = NoteOperation.GetNotesBySartLine(startline, cid);
-        int isDing = int.Parse(Request.QueryString["isDing"].ToString()); 
-        if (isDing == 3) 
+	int isDing= int.Parse(Request.QueryString["isDing"].ToString());
+        if ( isDing == 3) 
         {
             SmallScript.MessageBox(Page, "同一用户不能顶或踩同一个评论两次！");
         }
