1f136710d73c2912162fa2fa946a0e577c534480
陈一枭 <yhtt2020@foxmail.com>
2013-04-14 16:29:27 +0800

    实现提示

---
+++ CheckSuggest.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CheckSuggest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataBase db = new DataBase();
+        string rs = "";
+        DataSet ds_rs = db.ExCommand("select * from tb_note where startline=" + Request.QueryString["startline"].ToString() +
+            " and  recommend=1");
+        if (ds_rs.Tables[0].Rows.Count > 0)
+        {
+            rs = "1";
+        }
+        else
+        {
+            rs = "0";
+        }
+        Response.Write("{\"data\":\"" + Request.QueryString["startline"].ToString() + "\",\"status\":\"" + rs + "\",\"count\":\"" +
+            Request.QueryString["count"].ToString() + "\"}");
+    }
+    public class result
+    {
+        int data = 0;
+        int stat = 0;
+        public result(int data, int stat)
+        {
+            this.data = data;
+            this.stat = stat;
+        }
+    }
+}
